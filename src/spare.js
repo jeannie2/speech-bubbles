7406e03e2a3fdd585bbffb9d89937602caeb0871
jeannie2 <j.nam@hotmail.com>
2022-11-26 22:26:10 +0800

    Initial commit

---
+++ src/spare.js
@@ -0,0 +1,7 @@
+/* const utter = new SpeechSynthesisUtterance(text);
+
+        utter.addEventListener("boundary", (evt) => {
+          if (evt.charIndex >= index) {
+            console.log("EGG");
+          }
+        }); */
