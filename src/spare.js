6a1af548c7827a17cd94cc70118864bb12a323cc
jeannie2 <j.nam@hotmail.com>
2022-11-26 22:35:21 +0800

    Delete src directory

--- src/spare.js
+++
@@ -1,7 +0,0 @@
-/* const utter = new SpeechSynthesisUtterance(text);
-
-        utter.addEventListener("boundary", (evt) => {
-          if (evt.charIndex >= index) {
-            console.log("EGG");
-          }
-        }); */
